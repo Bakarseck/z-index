bccf72d375fc66fafe0995979ea97d90521c08f3
aboubakdiallo <babacardiallo2121@gmail.com>
2024-04-27 16:42:54 +0000

    landing page done

---
+++ src/components/BodyLanding.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function BodyLanding() {
+  return (
+    <div className="h-[80vh]">
+      <main className="flex items-center justify-between px-[50px] h-[600px]">
+        <div className="p-6 mr-10 text-black rounded-lg shadow-lg w-[500px]">
+          <h2 className="mb-2 text-2xl font-bold">Bienvenue sur DAARAY KOCC</h2>
+          <p className="mb-4">
+            {" "}
+            L'école en ligne où chaque défi enseigne une leçon.
+          </p>
+          <p className="text-sm">
+            Plongez dans notre univers ludique et éducatif, où apprendre devient
+            un voyage passionnant à travers l’histoire, la géographie et les
+            langues du Sénégal. <br/> Prêt à jouer pour apprendre et découvrir le
+            meilleur de l’éducation culturelle?
+          </p>
+          <button className="px-4 py-2 mt-4 text-white bg-signupBtn rounded-full w-[130px] h-[40px] bg-cover"></button>
+        </div>
+
+        <div className="p-6 text-black rounded-lg shadow-md">
+          <img src="assets/landing.PNG" className="w-96 rounded-lg" />
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import BodyLanding from "@/components/BodyLanding";
+import Footer from "@/components/Footer";
 
 export default function HomePage() {
   return (
@@ -9,57 +12,17 @@ export default function HomePage() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kavoon&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <div className="min-h-screen font-sans w-[90%] m-[5%] border-2 border-yellow-300  bg-opacity-10 shadow-lg backdrop-blur-100  rounded-lg">
-        <header className="flex items-center justify-between px-6 py-4">
-          <img
-            src="assets/logo-daaraayKocc.png"
-            alt="Daaray Kocc"
-            className="h-[100px]"
-          />
-          <nav>
-            <ul className="flex space-x-4">
-              <li>
-                <a href="#" className="text-yellow-500 hover:text-yellow-600">
-                  À propos de nous
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-yellow-500 hover:text-yellow-600">
-                  Connexion
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-yellow-500 hover:text-yellow-600">
-                  Inscription
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>
 
-        <main className="flex items-center justify-center h-screen">
-          <div className="p-6 mr-10 text-black bg-yellow-400 rounded-lg shadow-lg w-[500px]">
-            <h2 className="mb-2 text-2xl font-bold">TEXT</h2>
-            <p className="mb-4">TEXT</p>
-            <p className="text-sm">
-              diam nisi sit amet erat. Duis semper. Duis arcu massa, scelerisque
-              vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in
-              risus volutpat. Vestibulum ante ipsum primis
-            </p>
-            <button className="px-4 py-2 mt-4 text-white bg-red-500 rounded-full hover:bg-red-600">
-              Je m'inscris
-            </button>
-          </div>
-
-          <div className="p-6 text-black rounded-lg shadow-md">
-            <img src="assets/landing.PNG" className="w-96 rounded-lg" />
-          </div>
-        </main>
+      <div className="h-[79vh] font-sans w-[85%] mx-[7.5%] my-[5vh] border-2 border-yellow-300  bg-glass  rounded-lg">
+        <Navbar />
+        <BodyLanding />
       </div>
-      <footer className="py-6 text-xs text-center text-white">
-        ©2024 - daaraay kocc <i className="text-red-500 fas fa-heart"></i>
-      </footer>
+      <Footer />
     </>
   );
 }
