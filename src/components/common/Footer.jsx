60bac5f31fb6b5491f2f7b2ac1564f78da626373
bakseck <seck.bakar@ugb.edu.sn>
2024-04-27 21:23:59 +0000

    stable

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 
 export default function Footer() {
-<<<<<<< Updated upstream
-  return (
-    <footer className="py-6 text-xs text-center text-white h-[10vh] text-[22px] flex flex-row justify-center italic">
-      <span style={{ fontFamily: "Kavoon, cursive" }}>
-        ©2024 - daaraay kocc
-      </span>
-      <img src="../assets/senegal.png" className="w-[20px] ml-3" />
-    </footer>
-  );
-=======
     return (
         <footer className="absolute bottom-0 left-1/2 transform -translate-x-1/2 py-6 text-xs text-center text-white text-[18px] flex items-center justify-center italic">
             <span style={{ fontFamily: "Kavoon, cursive" }}>
@@ -19,5 +9,4 @@ export default function Footer() {
             <img src="/assets/senegal.png" alt="Senegal Flag" className="w-[20px] ml-3" />
         </footer>
     );
->>>>>>> Stashed changes
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,4 @@
 module.exports = {
-<<<<<<< Updated upstream
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "daaray-primary": "#E5E7EB", // replace with your color
-        "daaray-secondary": "#1F2937", // replace with your color
-        glass: "rgb(225, 225, 225, 0.2)",
-        // Add other custom colors as needed
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "signupBtn": "url(../../public/assets/registerBtn.png)",
-        "loginBtn": "url(/../../../assets/loginBtn.png)",
-        "registerBtn": "url(/../../../assets/registerFormBtn.png)",
-      },
-=======
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -44,7 +21,6 @@ module.exports = {
                 "signupBtn": "url(../../public/assets/registerBtn.png)",
             },
         },
->>>>>>> Stashed changes
     },
     plugins: [],
 };
