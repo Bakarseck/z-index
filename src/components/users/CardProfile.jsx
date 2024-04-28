84d0c7077fc7ccc8abda4fc3025a861110861323
aboubakdiallo <babacardiallo2121@gmail.com>
2024-04-28 12:16:13 +0000

    card

--- src/components/users/CardProfile.jsx
+++ src/components/users/CardProfile.jsx
@@ -2,25 +2,25 @@ import Image from "next/image";
 import DateComponent from "../common/DateComponent";
 
 export default function CardProfile({ user }) {
-    return (
-        <div className="max-w-4xl p-6 mx-auto mt-10 bg-gradient-to-r from-[#FE9F47] to-[#F8533D] rounded-lg shadow-md">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <Image
-                        src="/assets/avatar.png"
-                        alt={`${user.username}'s avatar`}
-                        width={100}
-                        height={100}
-                        className="rounded-full"
-                    />
-                    <div className="ml-4">
-                        <h2 className="text-xl font-bold">{user.username}</h2>
-                        <p className="text-sm">@{user.username.toLowerCase()}</p>
-                        <p className="text-xs">lvl: 1</p>
-                    </div>
-                </div>
-                <DateComponent />
-            </div>
+  return (
+    <div className="max-w-4xl p-6 mx-auto mt-10 bg-gradient-to-r from-[#FE9F47] to-[#F8533D] rounded-lg shadow-md">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src="/assets/avatar.png"
+            alt={`${user.username}'s avatar`}
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <div className="ml-4">
+            <h2 className="text-xl font-bold">{user.username}</h2>
+            <p className="text-sm">@{user.username.toLowerCase()}</p>
+            <p className="text-xs">lvl: 1</p>
+          </div>
         </div>
-    );
+        <DateComponent />
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                kavoon: ['Kavoon', 'cursive'],
-            },
-            colors: {
-                "daaray-primary": "#E5E7EB",
-                "secondary": "#636363",
-                glass: "rgb(225, 225, 225, 0.2)",
-            },
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-                "signupBtn": "url(../../public/assets/registerBtn.png)",
-            },
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        kavoon: ["Kavoon", "cursive"],
+      },
+      colors: {
+        "daaray-primary": "#E5E7EB",
+        secondary: "#636363",
+        glass: "rgb(225, 225, 225, 0.2)",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        signupBtn: "url(../../public/assets/registerBtn.png)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
